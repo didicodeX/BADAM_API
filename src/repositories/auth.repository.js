bed90eb4a489890c0c9a6f95025c6a622279738b
Dylane <tanolamene@gmail.com>
2025-04-15 21:17:24 -0300

    fix(modele User)

--- src/repositories/auth.repository.js
+++ src/repositories/auth.repository.js
@@ -1,10 +1,13 @@
 import { User } from "../models/user.model.js";
 
-export const saveUser = (userData) => {
-  const user = new User(userData);
-  return user.save();
+export const createUser = (userData) => {
+  return User.create(userData);
 };
 
 export const findUserById = async (userId) => {
   return await User.findById(userId).select("-password");
+};
+
+export const findByEmail = async (email) => {
+  return await User.findOne({ email });
 };
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -4,8 +4,7 @@ import * as authRepo from "../repositories/auth.repository.js";
 
 export const register = async (userData) => {
   // Vérifie si l'utilisateur existe déjà
-  const email = userData.email;
-  const existingUser = await User.findOne({ email });
+  const existingUser = await authRepo.findByEmail(userData.email);
   if (existingUser) throw new Error("Email already registered");
 
   // Hashage sécurisé du mot de passe
@@ -13,26 +12,23 @@ export const register = async (userData) => {
   userData.password = hashedPassword;
 
   // Enregistre l'utilisateur avec le mot de passe haché
-  return await authRepo.saveUser(userData);
+  return await authRepo.createUser(userData);
 };
 
 export const login = async (email, password) => {
-  const existUser = await User.findOne({ email });
+  const existUser = await authRepo.findByEmail(email);
 
-  if (!existUser) {
-    throw new Error("Utilisateur introuvable");
-  }
+  if (!existUser) throw new Error("Utilisateur introuvable");
 
-  const isPasswordCorrect = await bcrypt.compare(password, existUser.password);
-  if (!isPasswordCorrect) {
-    throw new Error("Mot de passe incorrect");
-  }
+  const isPasswordCorrect = bcrypt.compare(password, existUser.password);
+  if (!isPasswordCorrect) throw new Error("Mot de passe incorrect");
 
   const accessToken = jwt.sign(
     {
       id: existUser._id,
       email: existUser.email,
       name: existUser.name,
+      role: existUser.role,
     },
     process.env.JWT_SECRET,
     { expiresIn: "24h" }
