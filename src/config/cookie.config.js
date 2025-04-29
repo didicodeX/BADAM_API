a0cbdcfb144408267dddc2d709a04421f251c1a4
Dylane <tanolamene@gmail.com>
2025-04-29 07:53:24 -0300

    fix: update sameSite attribute in cookieOptions for better compatibility

--- src/config/cookie.config.js
+++ src/config/cookie.config.js
@@ -2,8 +2,8 @@ export const isProd = process.env.NODE_ENV === "production";
 
 export const cookieOptions = {
   httpOnly: true,
+  sameSite: isProd ? "None" : "Lax", // ✅ Lax en dev (accepte sur même site)
   secure: isProd,
-  sameSite: "None",
   domain: isProd ? process.env.COOKIE_DOMAIN : undefined,
   maxAge: 1000 * 60 * 60 * 24,
 };
